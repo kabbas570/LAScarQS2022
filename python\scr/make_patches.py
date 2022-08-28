04ecb998ba17c8779961a1ef849f35b75d2947ca
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2022-08-28 18:39:54 +0100

    Update make_patches.py

--- python\scr/make_patches.py
+++ python\scr/make_patches.py
@@ -4,18 +4,18 @@ import glob
 
 names=[]
 img_files = []
-for infile in sorted(glob.glob(r'C:\My_Data\sateg0\task_1_both_data\task1_2d\valid\img/*.npy')):
+for infile in sorted(glob.glob(r'/data/scratch/acw676/new_data/task1/valid/img/*.npy')):
     img_files.append(infile)
     names.append(infile[46:-4])
     
     
 sc_files=[]
 for n in names:
-    sc_files.append(r'C:\My_Data\sateg0\task_1_both_data\task1_2d\valid\sc_gt/'+n+'sc_gt.npy')
+    sc_files.append(r'/data/scratch/acw676/new_data/task1/valid/sc_gt/'+n+'sc_gt.npy')
     
     
-path=r'C:\My data\temp_data\patches\train\img'
-path1=r'C:\My data\temp_data\patches\train\gt'
+path_img='/data/scratch/acw676/new_data/patches/valid/img/'
+path_gt='/data/scratch/acw676/new_data/patches/valid/gt/'
 
 
 def find_(img,sc_gt,ID):
@@ -30,11 +30,11 @@ def find_(img,sc_gt,ID):
                    if np.sum(crop_gt)>=20:
                        count=count+1
                        if (count % 20 ==0):
-                           filename1 = os.path.join(path1,ID+'_'+str(count+1))
+                           filename_img = os.path.join(path_img,ID+'_'+str(count+1))
                            crop_img=img[y-24:y+24,x-24:x+24]
-                           filename = os.path.join(path,ID+'_'+str(count+1))
-                           np.save(filename1, crop_gt)
-                           np.save(filename, crop_img)                  
+                           filename_gt = os.path.join(path_gt,ID+'_'+str(count+1))
+                           np.save(filename_gt, crop_gt)
+                           np.save(filename_img, crop_img)                  
 for i in range(880):
     print(i)
     img=np.load(img_files[i])
